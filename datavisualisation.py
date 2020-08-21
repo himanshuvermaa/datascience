9b840253fc00630a1243ca416fbba4f6330f6349
Himanshu Verma <59117342+himanshuvermaa@users.noreply.github.com>
2020-08-21 17:59:38 +0530

    Update datavisualisation.py

--- datavisualisation.py
+++ datavisualisation.py
@@ -1,16 +1,11 @@
+  
 import pandas as pd
 import csv
+import plotly.express as px
 import plotly.graph_objects as go
 
-df = pd.read_csv("datav.csv")
-student_df =df.loc[df['student_id'] == "TRL_987"]
-print(df.groupby("level")["attempt"].mean())
-
-
-fig = go.Figure(go.Bar(
-          x=student_df.groupby("level")["attempt"].mean(),
-          y=['level1','level2','level3','level4'],
-          orientation= 'h'))
-fig.show()          
-            
+df = pd.read_csv("data.csv")
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
+fig.show()
              
